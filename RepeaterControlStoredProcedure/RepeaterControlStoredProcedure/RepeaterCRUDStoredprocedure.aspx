84aaad7d981c6c3815c90fee3c78e66c20daf110
Vaibhav Kumar <66525224+vaibhav5219@users.noreply.github.com>
2021-12-29 17:13:14 +0530

    Initial Commit
    Repeater Control with stored procedure

---
+++ RepeaterControlStoredProcedure/RepeaterControlStoredProcedure/RepeaterCRUDStoredprocedure.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RepeaterCRUDStoredprocedure.aspx.cs" Inherits="RepeaterControlStoredProcedure.RepeaterCRUDStoredprocedure" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Repeater ID="Repeater1" runat="server" OnItemCommand="Repeater1_ItemCommand">
+    <HeaderTemplate>
+        <table  border="1" cellpadding="0" cellspacing="0">
+            <tr>
+                <th scope="col" style="width: 120px">
+                    Name
+                </th>
+                <th scope="col" style="width: 100px">
+                    Address
+                </th>
+                <th scope="col" style="width: 120px">
+                    Email
+                </th>
+                <th scope="col" style="width: 100px">
+                    Mobile
+                </th>
+                <th scope="col" style="width: 120px">
+                    Password
+                </th>
+                <th scope="col" style="width: 150px">
+                </th>
+            </tr>
+    </HeaderTemplate>
+    <ItemTemplate>
+        <tr>
+            <td>
+                <asp:Label ID="lblUserId" runat="server" Text='<%# Eval("UserId") %>' Visible = "false" />
+                <asp:Label ID="lblUserName" runat="server" Text='<%# Eval("UserName") %>' />
+                <asp:TextBox ID="txtUserName" runat="server" Width="120" Text='<%# Eval("UserName") %>'
+                    Visible="false" />
+            </td>
+            <td>
+                <asp:Label ID="lblAddress" runat="server" Text='<%# Eval("UserAddress") %>' />
+                <asp:TextBox ID="txtAddress" runat="server" Width="120" Text='<%# Eval("UserAddress") %>'
+                    Visible="false" />
+            </td>
+            <td>
+                <asp:Label ID="Label1" runat="server" Text='<%# Eval("UserEmail") %>' />
+                <asp:TextBox ID="TextBox1" runat="server" Width="120" Text='<%# Eval("UserEmail") %>'
+                    Visible="false" />
+            </td>
+            <td>
+                <asp:Label ID="Label2" runat="server" Text='<%# Eval("Usermobile") %>' />
+                <asp:TextBox ID="TextBox2" runat="server" Width="120" Text='<%# Eval("Usermobile") %>'
+                    Visible="false" />
+            </td>
+            <td>
+                <asp:Label ID="Label3" runat="server" Text='<%# Eval("Password") %>' />
+                <asp:TextBox ID="TextBox3" runat="server" Width="120" Text='<%# Eval("Password") %>'
+                    Visible="false" />
+            </td>
+            <td>
+                <asp:LinkButton ID="lnkEdit" Text="Edit  | " runat="server" OnClick="OnEdit" />
+                <asp:LinkButton ID="lnkUpdate" Text="Update  |  " runat="server" Visible="false" OnClick="OnUpdate" />
+                <asp:LinkButton ID="lnkCancel" Text="Cancel" runat="server" Visible="false" OnClick="OnCancel" />
+                <asp:LinkButton ID="lnkDelete" Text="Delete" runat="server" OnClick="OnDelete" OnClientClick="return confirm('Do you want to delete this row?');" />
+            </td>
+        </tr>
+    </ItemTemplate>
+    <FooterTemplate>
+        </table>
+    </FooterTemplate>
+</asp:Repeater>
+        <br />
+<table border="1" cellpadding="0" cellspacing="0" >
+    <tr>
+        <td style="width: 150px">
+            User Name:<br />
+            <asp:TextBox ID="txtName" runat="server" Width="140" />
+        </td>
+        <td style="width: 150px">
+            Address:<br />
+            <asp:TextBox ID="TextAddress" runat="server" Width="140" />
+        </td>
+        <td style="width: 150px">
+            User Email:<br />
+            <asp:TextBox ID="TextEmail" runat="server" Width="140" />
+        </td>
+        <td style="width: 150px">
+            Mobile:<br />
+            <asp:TextBox ID="Textmobile" runat="server" Width="140" />
+        </td>
+        <td style="width: 150px">
+            Password:<br />
+            <asp:TextBox ID="txtPassword" runat="server" Width="140" />
+        </td>
+        <td style="width: 100px">
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="Insert_Data" />
+        </td>
+    </tr>
+</table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ RepeaterControlStoredProcedure/RepeaterControlStoredProcedure/RepeaterCRUDStoredprocedure.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace RepeaterControlStoredProcedure
+{
+    public partial class RepeaterCRUDStoredprocedure : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindRepeater();
+            }
+        }
+        private void BindRepeater()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["Vaibhav test dbConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_CRUD"))
+                {
+                    cmd.Parameters.AddWithValue("@Action", "SELECT");
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            Repeater1.DataSource = dt;
+                            Repeater1.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+        protected void Insert_Data(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string address = TextAddress.Text;
+            string email = TextEmail.Text;
+            string mobile = Textmobile.Text;
+            string password = txtPassword.Text;
+
+            string constr = ConfigurationManager.ConnectionStrings["Vaibhav test dbConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_CRUD"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "INSERT");
+                    cmd.Parameters.AddWithValue("@UserName", name);
+                    cmd.Parameters.AddWithValue("@UserAddress", address);
+                    cmd.Parameters.AddWithValue("@UserEmail", email);
+                    cmd.Parameters.AddWithValue("@Usermobile", mobile);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindRepeater();
+        }
+        protected void OnEdit(object sender, EventArgs e)
+        {
+            //Find the reference of the Repeater Item.
+            RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+            this.ToggleElements(item, true);
+        }
+
+        private void ToggleElements(RepeaterItem item, bool isEdit)
+        {
+            //Toggle Buttons.
+            item.FindControl("lnkEdit").Visible = !isEdit;
+            item.FindControl("lnkUpdate").Visible = isEdit;
+            item.FindControl("lnkCancel").Visible = isEdit;
+            item.FindControl("lnkDelete").Visible = !isEdit;
+
+            //Toggle Labels.
+            item.FindControl("lblUserName").Visible = !isEdit;
+            item.FindControl("lblAddress").Visible = !isEdit;
+            item.FindControl("Label1").Visible = !isEdit;
+            item.FindControl("Label2").Visible = !isEdit;
+            item.FindControl("Label3").Visible = !isEdit;
+
+            //Toggle TextBoxes.
+            item.FindControl("txtUserName").Visible = isEdit;
+            item.FindControl("txtAddress").Visible = isEdit;
+            item.FindControl("TextBox1").Visible = isEdit;
+            item.FindControl("TextBox2").Visible = isEdit;
+            item.FindControl("TextBox3").Visible = isEdit;
+        }
+        protected void OnUpdate(object sender, EventArgs e)
+        {
+            //Find the reference of the Repeater Item.
+            RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+            int UserId = int.Parse((item.FindControl("lblUserId") as Label).Text);
+            string name = (item.FindControl("txtUserName") as TextBox).Text.Trim();
+            string address = (item.FindControl("txtAddress") as TextBox).Text.Trim();
+            string email = (item.FindControl("TextBox1") as TextBox).Text.Trim();
+            string mobile = (item.FindControl("TextBox2") as TextBox).Text.Trim();
+            string password = (item.FindControl("TextBox3") as TextBox).Text.Trim();
+
+            string constr = ConfigurationManager.ConnectionStrings["Vaibhav test dbConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_CRUD"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "UPDATE");
+                    cmd.Parameters.AddWithValue("@UserId", UserId);
+                    cmd.Parameters.AddWithValue("@userName", name);
+                    cmd.Parameters.AddWithValue("@UserAddress", address);
+                    cmd.Parameters.AddWithValue("@UserEmail", email);
+                    cmd.Parameters.AddWithValue("@Usermobile", mobile);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindRepeater();
+        }
+        protected void OnCancel(object sender, EventArgs e)
+        {
+            //Find the reference of the Repeater Item.
+            RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+            this.ToggleElements(item, false);
+        }
+        protected void OnDelete(object sender, EventArgs e)
+        {
+            //Find the reference of the Repeater Item.
+            RepeaterItem item = (sender as LinkButton).Parent as RepeaterItem;
+            int UserId = int.Parse((item.FindControl("lblUserId") as Label).Text);
+
+            string constr = ConfigurationManager.ConnectionStrings["Vaibhav test dbConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_CRUD"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Action", "DELETE");
+                    cmd.Parameters.AddWithValue("@UserId", UserId);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindRepeater();
+        }
+
+        protected void Repeater1_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+
+        }
+    }
+}
